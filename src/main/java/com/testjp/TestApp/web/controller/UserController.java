762226c0f7c62ead8ab2476c9c583907e36a79dc
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-09-27 13:00:18 +0200

    Consommation API REST 2

--- src/main/java/com/testjp/TestApp/web/controller/UserController.java
+++ src/main/java/com/testjp/TestApp/web/controller/UserController.java
@@ -6,7 +6,6 @@ import com.testjp.TestApp.web.dao.UserDao;
 import com.testjp.TestApp.web.exceptions.*;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
-// import java.util.Iterator;  
-// import java.util.ListIterator;  
-
-import javax.annotation.PostConstruct;
 
 @RestController
 
@@ -35,27 +29,6 @@ public class UserController {
     public UserController(UserDao userDoa){
         this.userDoa = userDoa;
     }
-
-	@Value("${message.welcome}")
-	private String messageWelcom;
-
-	@Value("${message.select}")
-	private String messageSelect;
-
-	@Value("${message.error}")
-	private String errorMessage;
-
-	@Value("${message.error.user4o4}")
-	private String userNotFound;
-
-	@Value("${message.error.wtf}")
-	private String wtf;
-
-	// @GetMapping(value = { "/", "/index" })
-	// public String index(Model model) {
-	// 	model.addAttribute("message", messageWelcom);
-	// 	return "index";
-	// }
 
 	@GetMapping
 	public List<User> userList() {
@@ -71,11 +44,6 @@ public class UserController {
         return user;
 	}
 
-	// @GetMapping(value = { "/addUser" })
-	// public UserForm showAddUserPage() {
-	// 	return new UserForm();
-	// }
-
 	@PostMapping(value = { "/add" })
 	public User saveUser(@RequestBody User user) {
 
@@ -86,6 +54,8 @@ public class UserController {
 
 		Boolean nameIsValid = name != null && name.length() > 0;
 		Boolean typeIsValid = type != null && type.length() > 0;
+
+
 		if (nameIsValid) {
 			if (typeIsValid) {
 				for (User userLoop : userDoa.findAll()){
@@ -101,7 +71,7 @@ public class UserController {
 	}
 
 	@DeleteMapping(value = { "/delete/{id}" })
-	public List<User> deletUser(Model model, @PathVariable int id) {
+	public List<User> deletUser(@PathVariable int id) {
 		List<User> users = userDoa.findAll();
 		User user = userDoa.findAll().stream()
 			.filter(x -> id == x.getId()).findFirst()
@@ -131,15 +101,10 @@ public class UserController {
 	@PutMapping(value = { "/edit/{idUser}" })
 	public User editUser(Model model, @ModelAttribute("UserForm") UserForm UserForm, @PathVariable int idUser) {
 
-		System.out.println(UserForm.getId());
-		System.out.println(UserForm.getName());
-		System.out.println(UserForm.getChampionType());
-		System.out.println(UserForm.getHp());
-		
-		int id = UserForm.getId();
+		// int id = UserForm.getId();
 		String name = UserForm.getName();
 		String type = UserForm.getChampionType();
-		int hp = UserForm.getHp();
+		// int hp = UserForm.getHp();
 
 		Boolean nameIsValid = name != null && name.length() > 0;
 		Boolean typeIsValid = type != null && type.length() > 0;
@@ -158,25 +123,5 @@ public class UserController {
 		}
 		throw new ErrorForm("Error in entering the form. (Name)");
 	}
-
-    // @GetMapping("/users")
-    // public String listeProduits() {
-    //     // Iterator<User> itr = userDoa.findAll().listIterator(); ;
-    //     String str = "";
-
-    //     // while(itr.hasNext()){  
-    //     //     str+=itr.next().toString()+"<br>";
-    //     //     System.out.println(itr.next());  
-    //     // }   
-    //     for (User user : userDoa.findAll()){
-    //         str+=user.toString()+"<br>";
-    //     }
-    //     int removLastBr = str.length()-4;
-    //     return str.substring(0, removLastBr);
-    // }
-
-    // @GetMapping(value = "/users/{id}")
-    // public String oneUser(@PathVariable int id) {
-    //     return userDoa.findById(id).toString();
-    // }
+	
 }
